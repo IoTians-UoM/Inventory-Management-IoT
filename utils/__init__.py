5f97e6d28f90289b7ad24894d41e7a607a0f5a86
Chethiya Nuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2025-02-26 23:32:39 +0530

    refactor: remove unused MessageData type import from utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,4 +2,4 @@ from .websocket_utility import WebSocketClient
 from .local_db_utility import LocalDBUtility
 from .logger import Logger
 from .models import Product, InventoryItem
-from .types import Mode, Message, MessageData, Type, Action
+from .types import Mode, Message, Type, Action
